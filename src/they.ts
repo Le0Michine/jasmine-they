952067a9f2035fdd03b0c852ed2fcd49e63b7883
Leo Michine <leo@Levs-MacBook-Pro.local>
2017-09-09 22:15:34 +0200

    updated types

--- src/they.ts
+++ src/they.ts
@@ -1,28 +1,27 @@
-type TheyFunction = <T extends any[]>(description: string, testCases: T[], callback: (args: T) => void) => void;
-
-export function they(): TheyFunction {
-    return _they(it);
+export function they<T extends any[] | any>(description: string, testCases: T[], assertion: (args: T, done?: DoneFn) => void): void {
+    return _they(it)(description, testCases, assertion);
 }
 
-export function xthey(): TheyFunction {
-    return _they(it);
+export function xthey<T extends any[] | any>(description: string, testCases: T[], assertion: (args: T, done?: DoneFn) => void): void {
+    return _they(xit)(description, testCases, assertion);
 }
 
-export function fthey(): TheyFunction {
-    return _they(it);
+export function fthey<T extends any[] | any>(description: string, testCases: T[], assertion: (args: T, done?: DoneFn) => void): void {
+    return _they(fit)(description, testCases, assertion);
 }
 
-function _they(test: Function): TheyFunction {
-    return <T extends any[]>(description: string, testCases: T[], callback: (args: T) => void) => {
+function _they(test: Function): <T extends any[] | any>(description: string, testCases: T[], assertion: (args: T, done?: DoneFn) => void) => void {
+    return <T extends any[] | any>(description: string, testCases: T[], assertion: (args: T, done?: DoneFn) => void) => {
         if (!testCases.length) {
             return;
         }
 
         return describe(`${description} (${testCases.length} ${testCases.length > 1 ? "cases" : "case"})`, () => {
             testCases.forEach(testCase => {
-                test(`${description} (${JSON.stringify(testCase)})`, () => {
-                    callback(testCase);
-                });
+                const assertionWrapper = assertion.length > 1
+                    ? (done: DoneFn) => assertion(testCase, done)
+                    : () => assertion(testCase);
+                test(`${description} (${JSON.stringify(testCase)})`, assertionWrapper);
             });
         });
     };
